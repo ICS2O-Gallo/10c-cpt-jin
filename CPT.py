959c33c6481d8974f3fa38bddb57b1e188c45c43
Terrajin391 <47609892+Terrajin391@users.noreply.github.com>
2019-05-31 12:57:35 -0400

    Update and rename CPT SO FAR to CPT.py

--- CPT.py
+++ CPT.py
@@ -1,4 +1,5 @@
 import arcade
+import os
 
 
 WIDTH = 640
@@ -40,9 +41,22 @@ def on_draw():
         arcade.draw_text("PLAY GAME", 225, 165, arcade.color.BLACK, 25, 200)
         arcade.draw_text("INSTRUCTIONS", 225, 90, arcade.color.BLACK, 19, 200)
 
+    if current_screen == "instructions":
+        arcade.set_background_color(arcade.color.BLACK)
+        arcade.draw_text("INSTRUCTIONS", 40, 375, arcade.color.WHITE, 50, 200, "left", "Calibri", True, False)
+        arcade.draw_line(40, 360, 600, 360, arcade.color.WHITE, 10)
+        arcade.draw_text("In this game, you will be using your wits and brains to try to escape through 4 rooms. You will only be using your mouse through the entirety of this game. You will be clicking on different things and trying to use clues to solve puzzles. You can do stuff like fuse clues together and you can double click on them to get more information on it", 60, 320, arcade.color.WHITE, 15, 250, "left", "arial", False, False)
+        mouse = arcade.Sprite("images/mouse_png.png", 0.15)
+        mouse.center_x = 450
+        mouse.center_y = 200
+        mouse.draw()
+
 
 def on_key_press(key, modifiers):
-    pass
+    global current_screen
+    if current_screen == "instructions":
+        if key == arcade.key.ESCAPE:
+            current_screen = "main menu"
 
 
 def on_key_release(key, modifiers):
@@ -50,9 +64,12 @@ def on_key_release(key, modifiers):
 
 
 def on_mouse_press(x, y, button, modifiers):
+    global current_screen
     if current_screen == "main menu":
         if x > 220 and x < 420 and y > 150 and y < 200:
-            print ("button is clicked")
+            print ("play game")
+        if x > 220 and x < 420 and y > 75 and y < 125:
+            current_screen = "instructions"
 
 if __name__ == '__main__':
     setup()
